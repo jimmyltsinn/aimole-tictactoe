94a970afe311b700ecce03e27b9511c8a84a1ce9
Jimmy Sinn <jimmysinn91@gmail.com>
2016-02-15 02:09:13 +0800

    Write python utility to simulate verdict-player interaction; Require fflush() in player program

--- util/run.py
+++ util/run.py
@@ -1,18 +1,55 @@
 #!/usr/local/python
 
 import sys
+import os
+import subprocess
+import json
+import io
 
-if len(sys.argv) < 3: 
-    print 'Usage: %s verdict p1.o p2.o ...'%(sys.argv[0])
+if len(sys.argv) < 3:
+    print 'Usage: %s verdict execP1 execP2 ...' % sys.argv[0]
     sys.exit(-1)
 
 print sys.argv
 
 verdictPath = sys.argv[1]
-playerList = sys.argv[2:]
+playerPath = sys.argv[2:]
+playerNumer = len(playerPath)
 
 print 'Verdict: %s' % verdictPath
-print 'Number of player: %d' % len(playerList)
-print 'Players: %s' % str(playerList)
+print 'Number of player: %d' % playerNumer
+print 'Players: %s' % str(playerPath)
 
-## TODO
+verdict = subprocess.Popen(['python', '-u', os.path.abspath(verdictPath)],
+                           stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+players = []
+for i in xrange(0, playerNumer):
+    players.append(subprocess.Popen(os.path.abspath(playerPath[i]),
+                   stdin=subprocess.PIPE, stdout=subprocess.PIPE))
+
+verdict.stdin.write(json.dumps({'command': 'start'}) + '\n')
+verdict.stdin.flush()
+
+while True:
+    verdictData = json.loads(verdict.stdout.readline())
+    # All stderr ignored
+    # verdictErr = verdict.stderr.readline()
+    print verdictData
+
+    if verdictData['action'] == 'next':
+        nextPlayer = players[verdictData['nextPlayer']]
+        nextPlayer.stdin.write(verdictData['writeMsg'])
+        playerData = nextPlayer.stdout.readline()
+
+        playerMsg = {}
+        playerMsg['player'] = verdictData['nextPlayer']
+        playerMsg['command'] = 'player'
+        playerMsg['time'] = 0
+        playerMsg['stdout'] = playerData
+        verdict.stdin.write(json.dumps(playerMsg) + '\n')
+    elif verdictData['action'] == 'stop':
+        print "Verdict: Stop"
+        break
+    elif verdictData['action'] == 'error':
+        print "Verdict: Error (%s) " % verdictData['errorMessage']
+        break
